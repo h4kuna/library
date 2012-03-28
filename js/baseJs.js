8bf9a2c345e6520901cf49044ceb8bce14d8454c
Milan Matejcek <milan.matejcek@gmail.com>
2012-03-28 23:13:35 +0200

    vylepseny placeholder

--- js/baseJs.js
+++ js/baseJs.js
@@ -68,55 +68,25 @@ $(document).ready(function(){
 	});
 
 	//placholder
-	jQuery.support.placeholder = false;
-	test = document.createElement('input');
-	if('placeholder' in test) {
-		jQuery.support.placeholder = true;
-	}
-
-	if(!$.support.placeholder) {
-		$('[placeholder]').focus(function () {
+	if(!('placeholder' in document.createElement('input'))) {
+		$('[placeholder]').blur(function () {
 			$this = $(this);
 			placeholder = $this.attr('placeholder');
 
-			if($this.attr('type') == 'text') {
-				if (placeholder != '' && $this.val() == placeholder) {
-					$this.val('').removeClass('hasPlaceholder');
-				}
-			}
-
-		}).blur(function () {
-			$this = $(this);
-			placeholder = $this.attr('placeholder');
-
-			switch($this.attr('type')) {
-				case 'password':
-					if(placeholder != '' && $this.val() == '')
-					{
-						$this.after( $('<input type="text">').val($this.attr('placeholder')).attr('_id', $this.attr('id')).bind('focus', function(){
-							$('#'+$(this).attr('_id')).css({'display': 'inline-block'}).focus();
-							$(this).remove();
-						})).css({
-							'display': 'none'
-						});
-					}
-					break;
-				default:
-					if (placeholder != '' && ($this.val() == '' || $this.val() == placeholder)) {
-						$this.val($this.attr('placeholder')).addClass('hasPlaceholder');
-					}
-					break;
+			if(placeholder != '' && $this.val() == '')
+			{
+				$this.after( $('<input class="hasPlaceholder" type="text">')
+					.val( $this.attr('placeholder')).attr('_id', $this.attr('id') )
+					.bind('focus', function(){
+						$this = $(this);
+						$('#' + $this.attr('_id')).css({
+							'display': 'inline-block'
+						}).focus();
+						$this.remove();
+					})).css( {
+					'display': 'none'
+				} );
 			}
 		}).blur();
 	}
-
-	//chrome autofill
-	if (navigator.userAgent.toLowerCase().indexOf("chrome") >= 0) {
-		$('input:-webkit-autofill').each(function(k, v){
-			$(v).css({
-				'background-color': 'transparent'
-			});
-		});
-	}
-
 })
