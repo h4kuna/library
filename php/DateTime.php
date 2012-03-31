416ec42af1c1c224eb011984a1c3dc48b508d319
Milan Matejcek <milan.matejcek@gmail.com>
2012-03-31 12:43:52 +0200

    dateTime sunday/monday

--- php/DateTime.php
+++ php/DateTime.php
@@ -2,29 +2,50 @@
 
 namespace Utility;
 
-final class DateTime extends \DateTime
+use Nette;
+
+class DateTime extends Nette\DateTime
 {
-    static protected $timeUnit = array('hour', 'min', 'sec');
+	const SQL_DATE = 'Y-m-d';
+	const SQL_DATETIME = 'Y-m-d H:i:s';
+	const CZECH_DATE = 'j. n. Y';
+	const CZECH_DATE_ZERO = 'd.m.Y';
 
-    const SQL_DATE = 'Y-m-d';
-    const SQL_DATETIME = 'Y-m-d H:i:s';
-    const CZECH_DATE = 'j.n.Y';
+	protected $outFormat = self::SQL_DATE;
 
-    public $outFormat = self::SQL_DATE;
+	public function __construct($time = 'now', $object = NULL)
+	{
+		parent::__construct($this->fixRelativeMove($time), $object);
+	}
 
-    public function __toString()
-    {
-        return $this->format($this->outFormat);
-    }
+	public function setOutFormat($val)
+	{
+		$this->outFormat = $val;
+	}
 
-    public static function timeString($time, $flag='+')
-    {
-        $array = explode(':', $time);
-        $out = NULL;
-        foreach($array as $k => $val)
-        {
-            $out .= $flag . $val .' '. self::$timeUnit[$k] .' ';
-        }
-        return $out;
-    }
+	public function __toString()
+	{
+		return $this->format($this->outFormat);
+	}
+
+	public function modify($modify)
+	{
+		return parent::modify($this->fixRelativeMove($modify));
+	}
+
+	private function fixRelativeMove($time)
+	{
+		$time = strtolower($time);
+		if (strstr($time, 'week') !== FALSE && !date('w')) {
+			$found = array();
+			$x = ' week';
+			preg_match('~(\w*)' . $x . '~U', $time, $found);
+			static $change = array('previous' => '-2', 'this' => 'previous', 'next' => 'this');
+			if (!is_numeric($found[1]) && isset($change[$found[1]])) {
+				$time = str_replace($found[1] . $x, $change[$found[1]] . $x, $time);
+			}
+		}
+		return $time;
+	}
+
 }
