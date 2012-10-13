6d1a6faf730e7ae98b39ec2fbd1341333ff5e643
Milan Matejcek <milan.matejcek@gmail.com>
2012-10-13 08:54:12 +0200

    přidán přepínač na zjištění statutu

--- h4kuna/git.sh
+++ h4kuna/git.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+STATUS='pull'
+
+case $1 in
+     -s|--status) STATUS='status';;
+esac
+
 cd `dirname "$0"`
 repositories=('gettext-latte' 'experimental' 'fio' 'iterators' 'static' 'object-wrapper' 'unit-conversion' 'date-time' 'curl' 'tests' 'exchange' 'number-format' 'mutex' 'file')
 
 
 for dir in ${repositories[*]}
 do
+    echo 
+    echo "$dir:"
     if [ -d $dir ]
     then
         cd $dir
-        git pull
+        git $STATUS
         cd ..
     else
         git clone "git@github.com:h4kuna/$dir.git"
